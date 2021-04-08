fc047f4e83de8f216fe551eb543b996b1ffcbf20
vinh <fangireking789@gmail.com>
2021-04-08 18:07:02 +0700

    D

---
+++ QuanLyPhongKham2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuanLyPhongKham2.MvcApplication" Language="C#" %>
---
+++ QuanLyPhongKham2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuanLyPhongKham2.Startup))]
+namespace QuanLyPhongKham2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
